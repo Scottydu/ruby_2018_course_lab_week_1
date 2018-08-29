20ab0475763362b11483ffe96321b989decd35f6
Scottydu <scottmillion@gmail.com>
2018-08-29 20:55:22 +0700

    Add controller photos

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+#  get 'photos/index'
+#  get 'photos/show'
+  resources :photos #create a route for the method(s) inside
+
+  root 'photos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

79bcfd7ebf412ea8f31fcaf2736006cb6acdd7bf
Scottydu <scottmillion@gmail.com>
2018-08-29 20:18:15 +0700

    Add the schema for db and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Photo.create(username: 'michaeljackson', caption: 'king of pop', created_at: 10.days.ago, likes_count: 1000, url: "https://pbs.twimg.com/media/Clusm_JWYAEj2H-.jpg:large")
+Photo.create(username: 'taylorswift', caption: 'young queen of pop', likes_count: 10, url: 'https://lh5.googleusercontent.com/-rotwX2rMVUM/AAAAAAAAAAI/AAAAAAABb28/WRGFcZkDV6Q/s0-c-k-no-ns/photo.jpg')
